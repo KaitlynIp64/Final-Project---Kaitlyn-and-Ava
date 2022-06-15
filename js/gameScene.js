e5d39ce6e5fa67661a88c779bb0e0860de411b0d
KaitlynIp64 <99354960+KaitlynIp64@users.noreply.github.com>
2022-06-15 17:19:59 +0000

    added code

--- js/gameScene.js
+++ js/gameScene.js
@@ -14,10 +14,10 @@ class GameScene extends Phaser.Scene {
    * create an alien
    */
   createAlien() {
-    const alienXLocation = Math.floor(Math.random() * 1920) + 1 // this will get a number between 1 and 1920
+    const alienYLocation = Math.floor(Math.random() * 1080) + 1 // this will get a number between 1 and 1080
     let alienXVelocity = Math.floor(Math.random() * 50) + 1 // this will get a number between 1 and 50
-    alienXVelocity *= Math.round(Math.random()) ? 1 : -1 // thiis will add minus sign in 50% of cases
-    const anAlien = this.physics.add.sprite(alienXLocation, -100, "alien")
+    alienXVelocity *= Math.round(Math.random()) ? 1 : -1 // this will add minus sign in 50% of cases
+    const anAlien = this.physics.add.sprite(1920, alienYLocation, "alien")
     anAlien.body.velocity.y = 200
     anAlien.body.velocity.x = alienXVelocity
     this.alienGroup.add(anAlien)
